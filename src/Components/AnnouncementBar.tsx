8d317857186fb6a97bb3dd9bbd4fe97b6022ca79
Mujtaba Hassan <mujhassan786@gmail.com>
2025-02-19 21:50:49 +0200

    enhancement:add notification bar below currency converter

---
+++ src/Components/AnnouncementBar.tsx
@@ -0,0 +1,11 @@
+const AnnouncementBar = ()=>{
+    return (
+        <div className="flex justify-center items-center mt-5">
+            <div className="text-lime-900  text-sm">
+                <a>Note: You can only convert from Euro (EUR) at this time.</a>
+            </div>
+        </div>
+    )
+}
+
+export default AnnouncementBar
--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,3 +1,4 @@
+import AnnouncementBar from '../../Components/AnnouncementBar'
 import Converter from '../../Components/Converter'
 import Header from '../../Components/Header'
 
@@ -6,6 +7,7 @@ const Home = () => {
     < >
       <Header />
       <Converter />
+      <AnnouncementBar />
     </>
   )
 }
